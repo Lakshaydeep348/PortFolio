487727e69b4a6f8923369e3a67fa24f736dfa57d
Lakshaydeep <1806lakshay@gmail.com>
2023-06-02 20:19:45 +0530

    final

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+const hamEl=document.querySelector(".last");
+const disEl=document.querySelector(".hamburger")
 
- 
+function noScroll()
+{
+    window.addEventListener("scroll",()=>{
+        window.scrollTo(0,0);
+        })
+}
+
+
+
+hamEl.addEventListener("click",()=>{
+     disEl.style.display="flex";
+     noScroll();
+})
+
+
+
 
 
